59a5cc33c5cfc589078dd5ad6d5b68e00f124c3d
kellykbutler <38898095+kellykbutler@users.noreply.github.com>
2019-09-27 01:02:30 +0000

    Delete security.js

--- index_files/security.js
+++
@@ -1,8 +0,0 @@
-/* Ensure that the parent is the website. */
-try {
-    if (self.location.href != top.location.href) {
-        top.location = self.location;
-    }
-} catch ( ex ) {
-    top.location = self.location;
-}
